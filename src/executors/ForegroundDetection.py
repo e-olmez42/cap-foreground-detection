ec37e38c7ee4055ef35d7dfaafc8ca1150d2e7b2
emin ölmez <e.olmez42@gmail.com>
2025-11-12 17:28:47 +0300

    Update ForegroundDetection.py

--- src/executors/ForegroundDetection.py
+++ src/executors/ForegroundDetection.py
@@ -1,9 +1,3 @@
-"""
-Foreground Detection using MOG2 (Mixture of Gaussians v2)
-
-This component applies background subtraction to extract moving objects (foreground)
-from static background using OpenCV’s BackgroundSubtractorMOG2.
-"""
 
 import os
 import cv2
@@ -22,45 +16,31 @@ class ForegroundDetection(Capsule):
     def __init__(self, request, bootstrap):
         super().__init__(request, bootstrap)
         self.request.model = PackageModel(**(self.request.data))
-
-        # Config parametreleri
         self.history = self.request.get_param("history")
         self.varThreshold = self.request.get_param("varThreshold")
         self.detectShadows = self.request.get_param("detectShadows")
         self.image = self.request.get_param("inputImage")
-
-        # MOG2 oluştur
         self.bg_subtractor = cv2.createBackgroundSubtractorMOG2(
             history=self.history,
             varThreshold=self.varThreshold,
             detectShadows=self.detectShadows
         )
+        self.detections = []
 
     @staticmethod
     def bootstrap(config: dict) -> dict:
-        """Initialize required runtime objects or cache"""
         return {}
 
     def foreground_mask(self, image):
-        """Uygulanan foreground extraction işlemi"""
         mask = self.bg_subtractor.apply(image)
         return mask
 
     def run(self):
-        """Ana çalıştırma fonksiyonu"""
         img = Image.get_frame(img=self.image, redis_db=self.redis_db)
-
-        # Ön plan maskesi oluştur
         fg_mask = self.foreground_mask(img.value)
-
-        # Maskeyi 3 kanala dönüştür (isteğe bağlı)
         fg_color = cv2.cvtColor(fg_mask, cv2.COLOR_GRAY2BGR)
-
-        # Çıktı olarak kaydet
         img.value = fg_color
         self.image = Image.set_frame(img=img, package_uID=self.uID, redis_db=self.redis_db)
-
-        # Model cevabı oluştur
         packageModel = build_response(context=self)
         return packageModel
 
