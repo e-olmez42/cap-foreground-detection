6782cfa98d928a5dfc9e246c8aef2bcf53ffa64f
emin ölmez <e.olmez42@gmail.com>
2025-11-15 15:52:21 +0300

    Update ForegroundDetection.py

--- src/executors/ForegroundDetection.py
+++ src/executors/ForegroundDetection.py
@@ -2,7 +2,6 @@ import os
 import cv2
 import sys
 
-
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../../../'))
 
 from sdks.novavision.src.media.image import Image
@@ -10,7 +9,7 @@ from sdks.novavision.src.base.capsule import Capsule
 from sdks.novavision.src.helper.executor import Executor
 from capsules.ForegroundDetection.src.utils.response import build_response
 from sdks.novavision.src.base.model import Detection, BoundingBox
-from capsules.ForegroundDetection.src.utils.utils import ModelLoader
+from capsules.ForegroundDetection.src.utils.ModelLoader import ModelLoader
 from capsules.ForegroundDetection.src.models.PackageModel import PackageModel
 
 
@@ -28,18 +27,22 @@ class ForegroundDetection(Capsule):
         return {"model": model}
 
     def foreground_mask(self, image):
-        mask = self.model.apply(image, learningRate=-1)
-        return mask
+        return self.model.apply(image, learningRate=-1)
 
     def run(self):
         img = Image.get_frame(img=self.image, redis_db=self.redis_db)
         frame = img.value
         fg_mask = self.foreground_mask(frame)
+
+        # Temizlik işlemleri
         kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
         _, motion_mask = cv2.threshold(fg_mask, 1, 255, cv2.THRESH_BINARY)
+
         motion_mask = cv2.morphologyEx(motion_mask, cv2.MORPH_OPEN, kernel, iterations=1)
         motion_mask = cv2.morphologyEx(motion_mask, cv2.MORPH_CLOSE, kernel, iterations=1)
+
         contours, _ = cv2.findContours(motion_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
         min_contour_area = 500
         average_object_confidence = 1.0
         self.detections = []
@@ -67,9 +70,8 @@ class ForegroundDetection(Capsule):
         img.value = fg_color
         self.image = Image.set_frame(img=img, package_uID=self.uID, redis_db=self.redis_db)
 
-        packageModel = build_response(context=self)
-        return packageModel
+        return build_response(context=self)
 
 
-if "__main__" == __name__:
+if __name__ == "__main__":
     Executor(sys.argv[1]).run()
