37d4204415d4d9064789c1209c0f2f7c406d5934
emin ölmez <e.olmez42@gmail.com>
2025-11-15 10:51:38 +0300

    Create utils.py

---
+++ src/utils/utils.py
@@ -0,0 +1,40 @@
+import cv2
+
+from sdks.novavision.src.base.logger import LoggerManager
+from sdks.novavision.src.base.application import Application
+
+
+class ModelLoader:
+
+    def __init__(self, config: dict):
+        self.config = config
+        self.application = Application()
+        self.logger = LoggerManager()
+        self.executor = self.application.get_param(config=config, name="ConfigExecutor")["name"]
+
+    def load_model(self):
+        model_type = self.application.get_param(config=self.config, name="Type")
+
+        varThreshold = self.application.get_param(config=self.config, name="varThreshold")
+        history = self.application.get_param(config=self.config, name="history")
+        detectShadows = self.application.get_param(config=self.config, name="detectShadows")
+
+        if model_type == "MOG2":
+            model = cv2.createBackgroundSubtractorMOG2(
+                history=history,
+                varThreshold=varThreshold,
+                detectShadows=detectShadows
+            )
+
+        elif model_type == "KNN":
+            model = cv2.createBackgroundSubtractorKNN(
+                history=history,
+                dist2Threshold=varThreshold,
+                detectShadows=detectShadows
+            )
+
+        else:
+            raise ValueError(f"Unsupported model type: {model_type}")
+
+        return model
+
