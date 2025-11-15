bf39bd8784e0e419b06fe2c61d55865ca84354b1
emin ölmez <e.olmez42@gmail.com>
2025-11-15 15:14:50 +0300

    Update ForegroundDetection.py

--- src/executors/ForegroundDetection.py
+++ src/executors/ForegroundDetection.py
@@ -1,15 +1,16 @@
-
 import os
 import cv2
 import sys
+
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../../../'))
 
 from sdks.novavision.src.media.image import Image
 from sdks.novavision.src.base.capsule import Capsule
 from sdks.novavision.src.helper.executor import Executor
-from capsules.ForegroundDetection.src.utils.utils import ModelLoader
 from capsules.ForegroundDetection.src.utils.response import build_response
+from sdks.novavision.src.base.model import Detection, BoundingBox
+from capsules.ForegroundDetection.src.utils.utils import ModelLoader
 from capsules.ForegroundDetection.src.models.PackageModel import PackageModel
 
 
@@ -24,18 +25,48 @@ class ForegroundDetection(Capsule):
     @staticmethod
     def bootstrap(config: dict) -> dict:
         model = ModelLoader(config=config).load_model()
-        return {"model":model}
+        return {"model": model}
 
     def foreground_mask(self, image):
-        mask = self.model.apply(image)
+        mask = self.model.apply(image, learningRate=-1)
         return mask
 
     def run(self):
         img = Image.get_frame(img=self.image, redis_db=self.redis_db)
-        fg_mask = self.foreground_mask(img.value)
-        fg_color = cv2.cvtColor(fg_mask, cv2.COLOR_GRAY2BGR)
+        frame = img.value
+        fg_mask = self.foreground_mask(frame)
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+        _, motion_mask = cv2.threshold(fg_mask, 1, 255, cv2.THRESH_BINARY)
+        motion_mask = cv2.morphologyEx(motion_mask, cv2.MORPH_OPEN, kernel, iterations=1)
+        motion_mask = cv2.morphologyEx(motion_mask, cv2.MORPH_CLOSE, kernel, iterations=1)
+        contours, _ = cv2.findContours(motion_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        min_contour_area = 500
+        average_object_confidence = 1.0
+        self.detections = []
+
+        fg_color = cv2.cvtColor(motion_mask, cv2.COLOR_GRAY2BGR)
+
+        for contour in contours:
+            area = cv2.contourArea(contour)
+            if area < min_contour_area:
+                continue
+
+            x, y, w, h = cv2.boundingRect(contour)
+            cv2.rectangle(fg_color, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+            detection = Detection(
+                boundingBox=BoundingBox(left=x, top=y, width=w, height=h),
+                confidence=round(float(average_object_confidence), 2),
+                classId=0,
+                classLabel="foreground",
+                imgUID=self.uID,
+                keyPoints=[]
+            )
+            self.detections.append(detection)
+
         img.value = fg_color
         self.image = Image.set_frame(img=img, package_uID=self.uID, redis_db=self.redis_db)
+
         packageModel = build_response(context=self)
         return packageModel
 
