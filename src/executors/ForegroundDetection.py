1a3a9b043b8156875eb69096c27b4ebb55a82ff5
emin ölmez <e.olmez42@gmail.com>
2025-12-31 13:22:56 +0300

    Update ForegroundDetection.py

--- src/executors/ForegroundDetection.py
+++ src/executors/ForegroundDetection.py
@@ -10,8 +10,8 @@ from sdks.novavision.src.base.capsule import Capsule
 from sdks.novavision.src.helper.executor import Executor
 from capsules.ForegroundDetection.src.utils.response import build_response
 from sdks.novavision.src.base.model import Detection, BoundingBox
-from capsules.ForegroundDetection.src.utils.utils import ModelLoader
 from capsules.ForegroundDetection.src.models.PackageModel import PackageModel
+from capsules.ForegroundDetection.src.utils.utils import ModelLoader
 
 
 class ForegroundDetection(Capsule):
@@ -27,52 +27,34 @@ class ForegroundDetection(Capsule):
 
     @staticmethod
     def bootstrap(config: dict) -> dict:
-        config = config.copy()
         model = ModelLoader(config=config).load_model()
+        return {"model": model}
 
-        return {
-            "model": model
-        }
-
+    # ------------------------------------------------
     def clean_mask(self, raw_mask):
-
-        if self.model_type == "MOG2":
-            kernel_open = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
-            kernel_close = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
-
-            _, mask = cv2.threshold(raw_mask, 100, 255, cv2.THRESH_BINARY)
-            mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel_open)
-            mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel_close)
-
-        elif self.model_type == "KNN":
-            _, mask = cv2.threshold(raw_mask, 100, 255, cv2.THRESH_BINARY)
-            kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
-            mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-            mask = cv2.dilate(mask, kernel, iterations=1)
-
-        else:
-            mask = raw_mask
-
+        _, mask = cv2.threshold(raw_mask, 100, 255, cv2.THRESH_BINARY)
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+        mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
         return mask
 
+    # ------------------------------------------------
     def foreground_mask(self, image):
-        mask = self.model.apply(image)
-        final_mask = self.clean_mask(mask)
-        return final_mask
+        raw_mask = self.model.apply(image)
+        return self.clean_mask(raw_mask)
 
+    # ------------------------------------------------
     def run(self):
         img = Image.get_frame(img=self.image, redis_db=self.redis_db)
-        frame = img.value
+        frame = img.value.astype(np.uint8)
 
-        img_uint8 = frame.astype(np.uint8)
-        fg_mask = self.foreground_mask(img_uint8)
+        fg_mask = self.foreground_mask(frame)
+        contours, _ = cv2.findContours(
+            fg_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+        )
 
-        contours, _ = cv2.findContours(fg_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-        average_object_confidence = 1.0
         self.detections = []
-
-        fg_color = cv2.cvtColor(fg_mask, cv2.COLOR_GRAY2BGR)
+        vis = cv2.cvtColor(fg_mask, cv2.COLOR_GRAY2BGR)
 
         for contour in contours:
             area = cv2.contourArea(contour)
@@ -80,13 +62,11 @@ class ForegroundDetection(Capsule):
                 continue
 
             x, y, w, h = cv2.boundingRect(contour)
-
-            cv2.rectangle(fg_color, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            cv2.putText(fg_color, "Moving Object", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            cv2.rectangle(vis, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
             detection = Detection(
                 boundingBox=BoundingBox(left=x, top=y, width=w, height=h),
-                confidence=round(float(average_object_confidence), 2),
+                confidence=1.0,
                 classId=0,
                 classLabel="foreground",
                 imgUID=self.uID,
@@ -94,8 +74,10 @@ class ForegroundDetection(Capsule):
             )
             self.detections.append(detection)
 
-        img.value = fg_color
-        self.image = Image.set_frame(img=img, package_uID=self.uID, redis_db=self.redis_db)
+        img.value = vis
+        self.image = Image.set_frame(
+            img=img, package_uID=self.uID, redis_db=self.redis_db
+        )
 
         return build_response(context=self)
 
